96e2712fba621bd9115ea15e1745fcf180506ac9
MatiasRodriguez30 <l390585@gmail.com>
2026-05-21 18:33:08 -0300

    Subida del sistema de seguridad PALA

---
+++ src/main/java/com/example/subsistemaSeguridad/SubsistemaSeguridadApplication.java
@@ -0,0 +1,13 @@
+package com.example.subsistemaSeguridad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SubsistemaSeguridadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SubsistemaSeguridadApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/subsistemaSeguridad/SubsistemaSeguridadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.subsistemaSeguridad;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SubsistemaSeguridadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
